a18a54de8fbe2c2f06c71f95b24908d4b6e125d8
Refat Jamil <rifatjamil54@gmail.com>
2023-12-17 22:11:29 +0600

    connect PostgreSQL locally

--- AuthAPI/AuthAPI/settings.py
+++ AuthAPI/AuthAPI/settings.py
@@ -85,12 +85,15 @@ WSGI_APPLICATION = 'AuthAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "authapi",
+        "USER": "refat",
+        "PASSWORD": "12345",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
